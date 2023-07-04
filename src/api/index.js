24a5de32674354794d723134a637f96a96b42991
Jai Grover <jaigrover9179@gmail.com>
2023-07-04 21:58:04 +0530

    First Commit

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const baseURL =
+  "https://api.weatherapi.com/v1/current.json?key=cc3816cbc690412fa75152705230407";
+
+export const getWeatherDataForCity = async (city) => {
+  const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
+  return await response.json();
+};
+
+export const getWeatherDataForLocation = async (lat, lon) => {
+  const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
+  return await response.json();
+};
