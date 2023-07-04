0301e1c460e78ba745eb32c23fbe02b26c271be9
jai9179 <135509243+jai9179@users.noreply.github.com>
2023-07-04 22:02:50 +0530

    Update index.js
    Hide Weather-Api-Key

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 const baseURL =
-  "https://api.weatherapi.com/v1/current.json?key=cc3816cbc690412fa75152705230407";
+  "https://api.weatherapi.com/v1/current.json?key=API-KEY";
 
 export const getWeatherDataForCity = async (city) => {
   const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
